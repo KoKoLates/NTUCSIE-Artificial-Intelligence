0c51a664f18c13aae09363dddcfd7e6c50f83c2b
KoKoLates <the21515@gmail.com>
2023-06-20 11:21:27 +0800

    feat: update linear model and decision tree

--- Hw3/hw3.py
+++ Hw3/hw3.py
@@ -1,7 +1,10 @@
-from collections import Counter
+
+import ipdb
 
 import numpy as np
 import pandas as pd
+
+from collections import Counter
 
 # set random seed
 np.random.seed(0)
@@ -42,9 +45,8 @@ def load_data() -> tuple[pd.DataFrame, pd.DataFrame]:
 
 
 # 2. Preprocessing functions
-def train_test_split(
-    df: pd.DataFrame, target: str, test_size: float = 0.3
-) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+def train_test_split(df: pd.DataFrame, target: str, test_size: float = 0.3
+                     ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     # Shuffle and split dataset into train and test sets
     df = df.sample(frac=1, random_state=42).reset_index(drop=True)
     split_idx = int(len(df) * (1 - test_size))
@@ -61,25 +63,41 @@ def train_test_split(
 
 
 def normalize(X: np.ndarray) -> np.ndarray:
-    # Normalize features to [0, 1]
+    """ Normalize features to [0, 1]
+    @param X: the date to be processed.
+    @return: date being normalized.
+    @rtype: np.ndarray
+    """
     # You can try other normalization methods, e.g., z-score, etc.
+    # Hint: 
+    # Calculate the min and max values for each feature (colume)
+    # Then, substract the min value and divided by the range (max - min)
     # TODO: 1%
-    raise NotImplementedError
+    X_normalize = np.empty((X.shape))
+    min_value, max_value = np.amin(X, axis=0), np.amax(X, axis=0)
+    for i, j in np.ndindex(X.shape):
+        X_normalize[i, j] = (X[i, j] - min_value[j]) / (max_value[j] - min_value[j])
+    
+    return X_normalize
 
 
 def encode_labels(y: np.ndarray) -> np.ndarray:
-    """
-    Encode labels to integers.
-    """
+    """ Encode labels to integers. """
+    # Hint:
+    # Create a dictionary that maps unique labels to integers.
+    # Then, use a list comprehension to replace the original labels with their 
+    # corresponding integer values.
     # TODO: 1%
-    raise NotImplementedError
+    return np.array(
+        [['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'].index(y[i]) \
+         for i in range(y.size)]
+    )
 
 
 # 3. Models
 class LinearModel:
-    def __init__(
-        self, learning_rate=0.01, iterations=1000, model_type="linear"
-    ) -> None:
+    def __init__(self, learning_rate: float=0.01, 
+                 iterations: int=1000, model_type: str="linear") -> None:
         self.learning_rate = learning_rate
         self.iterations = iterations
         # You can try different learning rate and iterations
@@ -95,29 +113,36 @@ class LinearModel:
         n_classes = len(np.unique(y))
         n_features = X.shape[1]
 
-        if self.model_type == "logistic":
-            pass
-        else:
-            pass
         # TODO: 2%
-        raise NotImplementedError
+        # Hint:
+        # Initialize the weights based on the model type (logistic or linear)
+        # The, update the weights using gradient descent weithin a loop for specific iterations
+        self.weights = np.zeros(n_features if self.model_type == 'linear' \
+                                else (n_features, n_classes))            
+        
+        for itr in range(self.iterations):
+            self.weights -= self.learning_rate * self._compute_gradients(X, y)
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         X = np.insert(X, 0, 1, axis=1)
-        if self.model_type == "linear":
-            # TODO: 2%
-            raise NotImplementedError
-        elif self.model_type == "logistic":
-            # TODO: 2%
-            raise NotImplementedError
+        # TODO: 4%
+        return X @ self.weights if self.model_type == 'linear' else \
+            np.argmax(self._softmax(X @ self.weights), axis=1)
 
     def _compute_gradients(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        n_samples, n_features = X.shape
+        n_classes = len(np.unique(y))
+
         if self.model_type == "linear":
             # TODO: 3%
-            raise NotImplementedError
+            return 1 / n_samples * (X.T @ (X @ self.weights - y))
         elif self.model_type == "logistic":
             # TODO: 3%
-            raise NotImplementedError
+            y_enc = np.zeros((n_samples, n_classes), dtype=int)
+            for i in range(n_samples):
+                y_enc[i, y[i]] = 1
+            
+            return -1 / n_samples * (X.T @ (y_enc - self._softmax(X @ self.weights)))
 
     def _softmax(self, z: np.ndarray) -> np.ndarray:
         exp = np.exp(z)
@@ -146,8 +171,22 @@ class DecisionTree:
 
         feature, threshold = self._find_best_split(X, y)
         # TODO: 4%
-        raise NotImplementedError
+        # Hint:
+        # Create a mask based on the best feature and threshold
+        # that could separate the smaples into two groups. Then, 
+        # recursively build the left and right child nodes of the current node.
+        n_samples, n_features = X.shape
+        x_left, x_right, y_left, y_right = [], [], [], []
+        for i in range(n_samples):
+            x_left.append(X[i]) if X[i, feature] <= threshold else x_right.append(X[i])
+            y_left.append(y[i]) if X[i, feature] <= threshold else y_right.append(y[i])
 
+        left_child = self._build_tree(
+            np.array(x_left), np.array(y_left), depth + 1
+        )
+        right_child = self._build_tree(
+            np.array(x_right), np.array(y_right), depth + 1
+        )
 
         return {
             "feature": feature,
@@ -162,10 +201,11 @@ class DecisionTree:
     def _create_leaf(self, y: np.ndarray):
         if self.model_type == "classifier":
             # TODO: 1%
-            raise NotImplementedError
+            values, counts = np.unique(y, return_counts=True)
+            return values[np.argmax(counts)]
         else:
             # TODO: 1%
-            raise NotImplementedError
+            return y.mean()
 
     def _find_best_split(self, X: np.ndarray, y: np.ndarray) -> tuple[int, float]:
         best_gini = float("inf")
@@ -201,12 +241,19 @@ class DecisionTree:
 
     def _gini_index(self, left_y: np.ndarray, right_y: np.ndarray) -> float:
         # TODO: 4%
-        raise NotImplementedError
+        def _gini_calc(y: np.ndarray) -> float:
+            p = np.array([(y == i).sum() / y.size for i in np.unique(y)])
+            return 1 - (p ** 2).sum()
+        
+        return (left_y.size * _gini_calc(left_y) + right_y.size * _gini_calc(right_y)) / (left_y.size + right_y.size)
 
 
     def _mse(self, left_y: np.ndarray, right_y: np.ndarray) -> float:
         # TODO: 4%
-        raise NotImplementedError
+        def _mse_calc(y: np.ndarray) -> float:
+            return np.square(y - y.mean()).mean()
+        
+        return (left_y.size * _mse_calc(left_y) + right_y * _mse_calc(right_y)) / (left_y.size + right_y.size)
 
     def _traverse_tree(self, x: np.ndarray, node: dict):
         if isinstance(node, dict):
@@ -241,9 +288,9 @@ class RandomForest:
 
 
 # 4. Evaluation metrics
-def accuracy(y_true, y_pred):
+def accuracy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     # TODO: 1%
-    raise NotImplementedError
+    return (y_pred == y_true).sum() / y_true.size
 
 
 def mean_squared_error(y_true, y_pred):
@@ -269,6 +316,8 @@ def main():
     decision_tree_classifier.fit(X_train, y_train)
     y_pred = decision_tree_classifier.predict(X_test)
     print("Decision Tree Classifier Accuracy:", accuracy(y_test, y_pred))
+
+    ipdb.set_trace()
 
     random_forest_classifier = RandomForest(model_type="classifier")
     random_forest_classifier.fit(X_train, y_train)
